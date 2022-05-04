8ce965a184836a99388c3dc7a7ba33b172e191b1
ptso <prestonmtso@gmail.com>
2022-05-03 22:27:01 -0700

    working etch

--- javascript.js
+++ javascript.js
@@ -1,5 +1,24 @@
-const div = document.createElement("div");
-div.textContent = "Etch a sketch";
 
-const body = document.getElementById("first")
-body.appendChild(div)
+function createDiv(text) {
+	for (let x=0; x<4; x++) {
+		const container = document.createElement("div")
+		container.classList.add("container"+x)
+		const body = document.getElementById("first");
+		body.appendChild(container);
+		for (let i=0; i<4; i++) {
+		const div = document.createElement("div");
+		div.textContent = text;
+		div.classList.add("square")
+		container.appendChild(div);
+		div.addEventListener("mouseenter", function( event ) {
+			event.target.style.color="orange";
+			setTimeout(function() {
+				console.log("timoeout")
+				event.target.style.color="";
+			}, 10000);
+		}, false);
+		}
+	}
+}
+
+createDiv("test");
